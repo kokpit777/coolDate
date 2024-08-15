dfa25e63a09ae4ba644fc9403ce5766a148b3ade
rafan <rafanasev530@gmail.com>
2024-08-15 14:04:43 +0700

    sigma commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int Sigma = 22;
+        String Date = "февраля";
+        int numone = 2022;
+        String SVO = "ВЕЛИКИЙ ДЕНЬ: ";
+        char milSim = 'Z';
+        System.out.println(SVO + Sigma + " " + Date + " " + numone + " " + milSim);
+
+    }
+}
